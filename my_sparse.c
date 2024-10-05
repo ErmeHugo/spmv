b415731931b97370cc30f526f2c6a094702f8155
ErmeHugo <sembinelli@cy-tech.fr>
2024-10-05 17:52:52 +0200

    Create sparse matrix multiplication

--- my_sparse.c
+++ my_sparse.c
@@ -1,6 +1,13 @@
 #include "spmv.h"
 
-int my_sparse()
+int my_sparse(const unsigned int n, const double mat[], double vec[], double result[])
 {
-  // code your own solver
+  for (int i = 0; i < n; i++){
+    double sum = 0;
+    for (int j = 0; j < n; j++){
+      sum += mat[n*i + j] * vec[j];
+    }
+    result[i] = sum;
+  }
+  return 0;
 }
