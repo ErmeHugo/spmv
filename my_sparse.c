ada58de6c3b9205ba5f1c87c504316d01fd0e24b
ErmeHugo <sembinelli@cy-tech.fr>
2024-11-11 15:37:17 +0100

    Added my_sparse_csc and coo

--- my_sparse.c
+++ my_sparse.c
@@ -1,7 +1,7 @@
 #include "spmv.h"
 
 // Function to multiply CSR matrix by vector
-int my_sparse(const double *values, const int *col_i, const int *offset, 
+int my_sparse_csr(const double *values, const int *col_i, const int *offset, 
               const double *vec, double *result, int size)
   {
     // Initialize the result vector to zero
@@ -17,3 +17,39 @@ int my_sparse(const double *values, const int *col_i, const int *offset,
       }
       return 1;
   }
+
+// Function to multiply CSC matrix by vector
+int my_sparse_csc(const double *values, const int *row_j, const int *offset, 
+              const double *vec, double *result, int size)
+  {
+    // Initialize the result vector to zero
+      for (int i = 0; i < size; ++i) {
+          result[i] = 0.0;
+      }
+
+      // Perform the matrix-vector multiplication using CSC format
+        for (int col = 0; col < size; ++col) {
+            for (int j = offset[col]; j < offset[col + 1]; ++j) {
+                result[row_j[j]] += values[j] * vec[col];
+            }
+        }
+      
+      return 1;
+  }
+
+// Function to multiply COO matrix by vector
+int my_sparse_coo(const double *values, const int *col_i, const int *row_i, 
+              const double *vec, double *result, int nnz, int size)
+{
+    // Initialize the result vector to zero
+    for (int i = 0; i < size; ++i) {
+        result[i] = 0.0;
+    }
+
+    // Perform the matrix-vector multiplication using COO format
+    for (int i = 0; i < nnz; ++i) {
+        result[row_i[i]] += values[i] * vec[col_i[i]];
+    }
+    
+    return 1;
+}
